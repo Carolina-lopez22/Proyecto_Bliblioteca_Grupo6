b40e371554d8f745ccf7691233bd928af0779d37
Shabathiel <schavezr5@miumg.edu.gt>
2026-05-25 16:15:03 -0600

    feat Utils: utilidad para formato de fechas

--- src/utils/Date.java
+++ src/utils/Date.java
@@ -1,5 +1,16 @@
 package utils;
 
-public class Date {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+public class Date {
+	public static LocalDate formatDate(CharSequence Date) {
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		try {
+			return LocalDate.parse(Date, formato);
+		} catch (DateTimeParseException e) {
+			return null;
+		}
+	}
 }
